f4c51f79b4b8ebd69ea27ada17e5ebd7ac10a385
Nipun Shrivats <nipunrawat16@gmail.com>
2025-05-11 16:48:01 +0530

    updates on 11-5-25, change no.2

--- react_tutorial(part-2)/src/App.jsx
+++ react_tutorial(part-2)/src/App.jsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
+import AppLayout from "./components/layout/AppLayout";
+// ---------------------------------------
 
+// 1. Normal routes
 export default function App() {
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/services", element: <Services /> },
-    { path: "/contact", element: <Contact /> },
-    { path: "/about", element: <About /> },
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/services", element: <Services /> },
+        { path: "/contact", element: <Contact /> },
+        { path: "/about", element: <About /> },
+      ],
+    },
   ]);
+
+  // 2. older version routes
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="/about" element={<About />} />
+  //       <Route path="/services" element={<Services />} />
+  //       <Route path="/contact" element={<Contact />} />
+  //     </Route>
+  //   )
+  // );
+
+  // ----------------------------------------------------
 
   return <RouterProvider router={router} />;
 }
---
+++ react_tutorial(part-2)/src/Notes.md
@@ -0,0 +1,4 @@
+<!-- 1. Routing : createBrowserRouter -->
+<!-- 2. Routing helper function (older version):createRoutesFromElements-->
+<!-- 3. AppLayout in React -->
+<!-- 4. Navigation Bar -->
