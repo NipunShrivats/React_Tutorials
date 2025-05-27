376c0ce8c27cb1110a21037f3dab29bd59da2aaf
Nipun Shrivats <nipunrawat16@gmail.com>
2025-05-27 14:35:40 +0530

    updates on 27-5-25, change no.2

--- react_part_4(react-app-axios)/src/components/Posts.jsx
+++ react_part_4(react-app-axios)/src/components/Posts.jsx
@@ -17,16 +17,30 @@ export default function Posts() {
 
   return (
     <>
-      <section>
-        <ul>
+      <section className="bg-black p-[2rem] border-4 border-white">
+        <ul className="flex flex-row flex-wrap gap-[1rem]">
           {apiData.map((curEle) => {
             const { id, title, body } = curEle;
             return (
-              <li key={id}>
-                <p>{title}</p>
-                <p>{body}</p>
-                <button>Edit</button>
-                <button>Delete</button>
+              <li
+                key={id}
+                className="border-1 rounded-[.8rem] flex-1/4 px-2 py-[1rem] bg-gradient-to-br from-slate-100 to-sky-100"
+              >
+                <p>{id}.</p>
+                <p className="h-[3.5rem] ">
+                  <span className="font-bold">Title:</span> {title}
+                </p>
+                <p className="h-[7rem]">
+                  <span className="font-bold">News:</span> {body}
+                </p>
+                <div className="flex gap-[1rem]">
+                  <button className="px-[2rem] py-[.3rem] rounded-[.2rem] bg-green-400 hover:bg-green-300 transition-all duration-300 cursor-pointer">
+                    Edit
+                  </button>
+                  <button className=" px-[2rem] py-[.3rem] rounded-[.2rem] bg-red-400 hover:bg-red-300 transition-all duration-300 cursor-pointer">
+                    Delete
+                  </button>
+                </div>
               </li>
             );
           })}
