b30b81aadbe2af9e5c4cf9a6166d9b1b6b68a849
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-11 12:11:41 +0530

    updates on 11-6-25, change no.1

---
+++ react_part_7(WebSite)/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import AppLayout from "./components/Layout/AppLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import Country from "./pages/Country";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contacts",
+        element: <Contacts />,
+      },
+      {
+        path: "country",
+        element: <Country />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
