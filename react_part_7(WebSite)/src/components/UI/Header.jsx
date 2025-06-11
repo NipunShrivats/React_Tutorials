48b22c9fd7270ca5e5c96d7b1af359f808a3aea0
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-11 13:55:47 +0530

    updates on 11-06-25, change no.02

--- react_part_7(WebSite)/src/components/UI/Header.jsx
+++ react_part_7(WebSite)/src/components/UI/Header.jsx
@@ -1,5 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="logo">
+            <NavLink to="/">WorldAtlas</NavLink>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to={"/"}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/about"}>About</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/contacts"}>Contacts</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/country"}>Country</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/"}></NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 }
