eda84bf051185102195e4f4eccf0fb4ddcbf9796
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-14 14:53:09 +0530

    updates on 14-06-25, change no.01

--- react_part_7(WebSite)/src/pages/Contacts.jsx
+++ react_part_7(WebSite)/src/pages/Contacts.jsx
@@ -1,5 +1,45 @@
 import React from "react";
 
 export default function Contacts() {
-  return <h1>Contacts</h1>;
+  // this is formData hook/api
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+    console.log(formInputData);
+  };
+  return (
+    <section className="section-contact">
+      <h2 className="container-title">Contact us</h2>
+      <div className="contact-wrapper container">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Enter your name"
+            name="username"
+            required
+            autoComplete="false"
+          />
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter your email"
+            name="email"
+            required
+            autoComplete="false"
+          />
+          <textarea
+            className="form-control"
+            rows="10"
+            placeholder="Enter your message"
+            name="message"
+            required
+            autoComplete="false"
+          ></textarea>
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
