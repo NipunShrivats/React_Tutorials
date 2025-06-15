83a6f5c86ec0d0696b85a295849e3403a343f8e5
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-15 14:43:24 +0530

    updates on 15-06-25, change no.03

---
+++ react_part_7(WebSite)/src/api/postApi.jsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const api = axios.create({
+  // https://restcountries.com/v3.1/all?fields=name,population,region,capital,flags
+  baseURL: "https://restcountries.com/v3.1",
+});
+
+// HTTP GET METHOD
+
+export const getCountryData = () => {
+  return api.get("/all?fields=name,population,region,capital,flags");
+};
--- react_part_7(WebSite)/src/pages/Country.jsx
+++ react_part_7(WebSite)/src/pages/Country.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useTransition } from "react";
 
 export default function Country() {
+  useTransition();
+  useEffect(() => {}, []);
   return <h1>Country</h1>;
 }
