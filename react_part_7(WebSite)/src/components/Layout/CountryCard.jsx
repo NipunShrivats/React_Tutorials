60bed2228ec614dae9a783b2b6c3e771074532fc
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-16 23:47:03 +0530

    updates on 16-06-25, change no.01

---
+++ react_part_7(WebSite)/src/components/Layout/CountryCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function CountryCard({ country }) {
+  const { flags, name, population, region, capital } = country;
+  return (
+    <>
+      <li className="country-card card">
+        <div className="container-card bg-white-box">
+          <img src={flags.svg} alt={flags.alt} />
+          <div className="countryInfo">
+            <p className="card-title">
+              {name.common.length > 10
+                ? name.common.slice(0, 10) + "..."
+                : name.common}
+            </p>
+            <p>
+              <span className="card-description">Region: </span>
+              {region}
+            </p>
+            <p>
+              <span className="card-description">Capital: </span>
+              {capital[0]}
+            </p>
+            <p>
+              <span className="card-description">Population: </span>
+              {population}
+            </p>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
--- react_part_7(WebSite)/src/pages/Country.jsx
+++ react_part_7(WebSite)/src/pages/Country.jsx
@@ -1,7 +1,32 @@
-import React, { useEffect, useTransition } from "react";
+import React, { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import Loader from "../components/UI/Loader";
+import CountryCard from "../components/Layout/CountryCard";
 
 export default function Country() {
-  useTransition();
-  useEffect(() => {}, []);
-  return <h1>Country</h1>;
+  const [isPending, startTransition] = useTransition(); //
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data);
+    });
+  }, []);
+
+  if (isPending) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <section className="country-section">
+        <ul className="grid grid-four-cols">
+          {countries.map((CurCountry, index) => {
+            return <CountryCard country={CurCountry} key={index} />;
+          })}
+        </ul>
+      </section>
+    </>
+  );
 }
