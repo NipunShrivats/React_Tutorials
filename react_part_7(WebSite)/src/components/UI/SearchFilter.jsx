f3c1a2bca9a614d1fb78913a95b55fa7a8dbee8f
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-20 13:20:08 +0530

    updates on 20-06-25, change no.02

---
+++ react_part_7(WebSite)/src/components/UI/SearchFilter.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import continentList from "../../api/continentList.json";
+
+export default function SearchFilter({
+  search,
+  setSearch,
+  filter,
+  setFilter,
+  countries,
+  setCountries,
+}) {
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    setSearch(event.target.value);
+  };
+  const handleSelectChange = (event) => {
+    event.preventDefault();
+    setFilter(event.target.value);
+  };
+
+  const sortCountries = (value) => {
+    const sortCountry = [...countries].sort((a, b) => {
+      return value === "asc"
+        ? a.name.common.localeCompare(b.name.common)
+        : b.name.common.localeCompare(a.name.common);
+    });
+    setCountries(sortCountry);
+  };
+
+  return (
+    <section className="section-searchFilter container">
+      <input
+        type="text"
+        placeholder="search"
+        value={search}
+        onChange={handleInputChange}
+      />
+
+      <div className="ascDes_btn">
+        <button onClick={() => sortCountries("asc")}>A-Z</button>
+        <button onClick={() => sortCountries("des")}>Z-A</button>
+      </div>
+      <div>
+        <select
+          className="select-section"
+          value={filter}
+          onChange={handleSelectChange}
+        >
+          <option value="All">All</option>
+          {continentList.map((curData) => {
+            const { id, name, countries } = curData;
+            return (
+              <option value={name} key={id}>
+                {name} ({countries} countries)
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </section>
+  );
+}
--- react_part_7(WebSite)/src/pages/Country.jsx
+++ react_part_7(WebSite)/src/pages/Country.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/postApi";
 import Loader from "../components/UI/Loader";
 import CountryCard from "../components/Layout/CountryCard";
+import SearchFilter from "../components/UI/SearchFilter";
 
 export default function Country() {
   const [isPending, startTransition] = useTransition(); //
   const [countries, setCountries] = useState([]);
 
-  const [search, setSearch] = useState();
-  const [filter, setFilter] = useState();
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
 
   useEffect(() => {
     startTransition(async () => {
@@ -20,12 +21,40 @@ export default function Country() {
   if (isPending) {
     return <Loader />;
   }
+  // console.log("search:", search);
+  // console.log("filter:", filter);
+
+  const searchCountry = (country) => {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search.toLowerCase());
+    }
+    return country;
+  };
+
+  const filterRegion = (country) => {
+    if (filter === "All") {
+      return country;
+    } else {
+      return country.region === filter;
+    }
+  };
+  const filterCountry = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
+  );
 
   return (
     <>
       <section className="country-section">
+        <SearchFilter
+          search={search}
+          setSearch={setSearch}
+          filter={filter}
+          setFilter={setFilter}
+          countries={countries}
+          setCountries={setCountries}
+        />
         <ul className="grid grid-four-cols">
-          {countries.map((CurCountry, index) => {
+          {filterCountry.map((CurCountry, index) => {
             return <CountryCard country={CurCountry} key={index} />;
           })}
         </ul>
