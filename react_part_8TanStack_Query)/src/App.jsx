402e79bafa50fa8c9b4e6872c82a5bfe7e7ccc45
Nipun Shrivats <nipunrawat16@gmail.com>
2025-06-22 23:56:31 +0530

    updates on 22-06-25, change no.04

--- react_part_8TanStack_Query)/src/App.jsx
+++ react_part_8TanStack_Query)/src/App.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
 
+import Home from "./components/pages/Home";
+import FetchOld from "./components/pages/FetchOld";
+import FetchRQ from "./components/pages/FetchRQ";
+
+// create router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/fetchold",
+        element: <FetchOld />,
+      },
+      {
+        path: "/fetchrq",
+        element: <FetchRQ />,
+      },
+    ],
+  },
+]);
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
