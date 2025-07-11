e333ebb823828d07525e886898bf8d365cd2bfad
NipunShrivats <nipunrawat16@gmail.com>
2025-07-11 14:41:24 +0530

    changes on Jul11, change no.05

---
+++ react_part_8(tanstack-query)/src/pages/FetchOld.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { fetchPosts } from "../API/Api";
+
+export default function FetchOld() {
+  const [posts, setPosts] = useState([]);
+
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+      res.status === 200 ? setPosts(res.data) : [];
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    getPostsData();
+  }, []);
+
+  return <>FetchOld</>;
+}
